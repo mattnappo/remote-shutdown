2dd907454555bfc54828c605d2f918740599d72f
xoreo <mattnappo@gmail.com>
2018-10-14 19:56:40 -0400

    EXPRESS WORKING

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ const SSH = require("simple-ssh");
 const path = require("path");
 
 var app = express();
+app.use(express.static(__dirname + "/static"));
 
+console.log(__dirname)
 app.get("/", function (req, res) {
-    res.sendFile(path.resolve("../index.html"));
+    res.sendFile(path.resolve("src/static/index.html"));
 });
 
 app.listen(3000);
