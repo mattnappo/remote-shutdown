4b14a56a6e6c26394f073c2b749360f0e4990ecd
xoreo <mattnappo@gmail.com>
2019-04-08 22:55:58 -0400

    Ready for testing

--- src/server.js
+++ src/server.js
@@ -19,6 +19,10 @@ app.post("/close", function (req, res, next) {
     turnoff(req.body.game);
 });
 
+app.post("/message", function (req, res, next) {
+    send_message(req.body.message);
+});
+
 var ssh = new SSH({
     host: server.host,
     user: server.user,
@@ -28,6 +32,28 @@ var ssh = new SSH({
 function is_running() {
     // Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
     // to enable the linux subsystem, run on chris' pc
+}
+
+function send_message(message) {
+    console.log(`sending message '${message}'`);
+    let task = 'x=msgbox("' + message + '“ ,16, “Message from big brother”)';
+    try {
+        ssh.exec("echo '" + task + "' > popup.vba", {
+            out: function(stdout) {
+                console.log(stdout + '\n');
+            }
+        }).start();
+
+        ssh.exec("cscript popup.vba", {
+            out: function(stdout) {
+                console.log(stdout + '\n');
+            }
+        }).start();
+    }
+    catch (err) {
+        console.error(err.message);
+    }
+    // x=msgbox("message body“ ,16, “WARNING”)
 }
 
 function kill(process, game) {
--- src/static/js/helpers.js
+++ src/static/js/helpers.js
@@ -5,3 +5,15 @@ function hide_modal(id) {
 function show_modal(id) {
     document.getElementById(id).style.display='block';
 }
+
+function new_message() {
+    let message = document.getElementById("message-to-send").value;
+    // alert(message);
+    
+    $.post("/message", {message: message}, function (res) {
+        console.log(`res: ${res}`);
+    });
+
+    hide_modal("new-message");
+    show_modal("sent");
+}
